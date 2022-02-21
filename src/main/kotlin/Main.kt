240e67e0ab89faf8c40eed9b70052ce4535abe76
Mark Fleschler <mark.f.accts@gmail.com>
2022-02-20 21:28:30 -0800

    Added cyrcle3 sketch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import processing.core.PApplet
 
 //fun main() = PApplet.main(Cyrcle1::class.java.name)
-fun main() = PApplet.main(Cyrcle2::class.java.name)
+//fun main() = PApplet.main(Cyrcle2::class.java.name)
+fun main() = PApplet.main(Cyrcle3::class.java.name)
